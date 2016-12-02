4121808d1000399bd09dbc396dfcaabea5f2c791
Kevin Ma <kevinma.sd@gmail.com>
2016-12-02 11:35:02 -0800

    Add Load Extension

--- interact/static/script.js
+++ interact/static/script.js
@@ -1,95 +1,105 @@
-function updateStatus(payload) {
-  $('.status').html(payload);
-}
+define(['jquery'], function ($) {
+  function updateStatus(payload) {
+    $('.status').html(payload);
+  }
 
-function handleRedirect(payload) {
-  $('.status').html('Redirecting you to ' + payload);
-  window.location.href = payload;
-}
+  function handleRedirect(payload) {
+    $('.status').html('Redirecting you to ' + payload);
+    window.location.href = payload;
+  }
 
-function updateLog(payload) {
-  $('.log').html(payload + '\n');
-}
+  function updateLog(payload) {
+    $('.log').html(payload + '\n');
+  }
 
-function showError(payload) {
-  updateStatus(payload);
-}
+  function showError(payload) {
+    updateStatus(payload);
+  }
 
-// Keep in sync with messages.py
-var messageHandlers = {
-  'LOG': updateLog,
-  'STATUS': updateStatus,
-  'REDIRECT': handleRedirect,
-  'ERROR': showError,
-};
-
-// Launches a socket connection with server-side, receiving status updates and
-// updating the page accordingly.
-function openStatusSocket(socket_args) {
-  var is_development = socket_args['is_development'];
-  var base_url = socket_args['base_url'];
-  var username = socket_args['username'];
-
-  // Constructs a URL that looks like:
-  //
-  // ws://<host>/<base_url>/socket/<username>?<params>
-  //
-  // Uses wss in production.
-  var url = [
-    is_development ? 'ws://' : 'wss://',
-    window.location.hostname,
-    ':',
-    window.location.port,
-    base_url,
-    'socket/',
-    username,
-    window.location.search,
-  ].join('');
-
-  var socket = new WebSocket(url);
-
-  socket.onopen = function() {
-    console.log('[Client] Connected to url: ' + url);
+  // Keep in sync with messages.py
+  var messageHandlers = {
+    'LOG': updateLog,
+    'STATUS': updateStatus,
+    'REDIRECT': handleRedirect,
+    'ERROR': showError,
   };
 
-  /**
-   * This function takes in a message from messages.py as a JSON string.
-   * It calls the corresponding handler for each message type.
-   */
-  socket.onmessage = function(event) {
-    message = JSON.parse(event.data);
+  // Launches a socket connection with server-side, receiving status updates and
+  // updating the page accordingly.
+  function openStatusSocket(socket_args) {
+    var is_development = socket_args['is_development'];
+    var base_url = socket_args['base_url'];
+    var username = socket_args['username'];
 
-    if (is_development) {
-      console.log(message);
-    }
+    // Constructs a URL that looks like:
+    //
+    // ws://<host>/<base_url>/socket/<username>?<params>
+    //
+    // Uses wss in production.
+    var url = [
+      is_development ? 'ws://' : 'wss://',
+      window.location.hostname,
+      ':',
+      window.location.port,
+      base_url,
+      'socket/',
+      username,
+      window.location.search,
+    ].join('');
 
-    var handler = messageHandlers[message.type];
-    handler(message.payload);
-  };
-}
+    var socket = new WebSocket(url);
 
-$(document).ready(function() {
-  $('.console_log').on('click', function() {
-    if ($('.log-container').is(':visible')) {
-      $('.log-container').hide();
-      $('.console_log').html('Show Console Log');
-    } else {
-      $('.log-container').show();
-      $('.console_log').html('Hide Console Log');
-    }
-    return false;
-  });
+    socket.onopen = function() {
+      console.log('[Client] Connected to url: ' + url);
+    };
 
-  $('.server-field').on('input', function() {
-    value = $(this).val();
-    $('.server-display').each(function() {
-      if (value == '') {
-        $(this).html('ds8.berkeley.edu');
-      } else {
-        $(this).html(value);
+    /**
+     * This function takes in a message from messages.py as a JSON string.
+     * It calls the corresponding handler for each message type.
+     */
+    socket.onmessage = function(event) {
+      message = JSON.parse(event.data);
+
+      if (is_development) {
+        console.log(message);
       }
-    });
-    $('.server-button').attr('href', 'http://' + value +
-      $('.server-input .reversed').contents()[2].textContent);
-  });
+
+      var handler = messageHandlers[message.type];
+      handler(message.payload);
+    };
+  }
+  var on_start = function () {
+    $(document).ready(function() {
+      $('.console_log').on('click', function() {
+        if ($('.log-container').is(':visible')) {
+          $('.log-container').hide();
+          $('.console_log').html('Show Console Log');
+        } else {
+          $('.log-container').show();
+          $('.console_log').html('Hide Console Log');
+        }
+        return false;
+      });
+
+      $('.server-field').on('input', function() {
+        value = $(this).val();
+        $('.server-display').each(function() {
+          if (value == '') {
+            $(this).html('ds8.berkeley.edu');
+          } else {
+            $(this).html(value);
+          }
+        });
+        $('.server-button').attr('href', 'http://' + value +
+          $('.server-input .reversed').contents()[2].textContent);
+      });
+    }) 
+  };
+  var load_ipython_extension = function () {
+    on_start();
+  };
+
+  return {
+    load_ipython_extension: load_ipython_extension,
+  };
 });
